a6943264d2dc492e1b7c5423a72e88f59402dc14
RiziqSusanto <riziq.susanto@gmail.com>
2024-12-12 21:59:33 +0700

    test

--- app/_utils/GlobalApi.jsx
+++ app/_utils/GlobalApi.jsx
@@ -5,7 +5,7 @@ const { default: axios } = require("axios");
 const API_KEY = process.env.NEXT_PUBLIC_STRAPI_API_KEY
 
 const axiosClient = axios.create({
-    baseURL: 'https://appointment-web-3ac4.onrender.com/api',
+    baseURL: 'https://localhost:1337/api',
     headers: {
         'Authorization': `Bearer ${API_KEY}`
     }
