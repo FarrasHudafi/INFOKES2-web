6149e6903fef37ffd3db7255789ffc38290da489
RiziqSusanto <riziq.susanto@gmail.com>
2024-12-12 16:58:33 +0700

    update

--- app/_utils/GlobalApi.jsx
+++ app/_utils/GlobalApi.jsx
@@ -5,7 +5,7 @@ const { default: axios } = require("axios");
 const API_KEY = process.env.NEXT_PUBLIC_STRAPI_API_KEY
 
 const axiosClient = axios.create({
-    baseURL: 'https://infokes2-1.onrender.com/api',
+    baseURL: 'http://localhost:1337/api',
     headers: {
         'Authorization': `Bearer ${API_KEY}`
     }
